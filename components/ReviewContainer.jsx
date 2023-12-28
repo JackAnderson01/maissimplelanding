3607a58c81b64cab37940a4a0b7fb93ae117fe19
jonathan-dignite <bilal.arshad@launchbox.pk>
2023-12-29 03:21:22 +0500

    Updated price module and updated content

--- components/ReviewContainer.jsx
+++ components/ReviewContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 
@@ -10,10 +10,7 @@ import Reviewcard from "./Reviewcard";
 import { fadeIn, slideIn, staggerContainer } from "../utils/motion.js";
 import { motion } from "framer-motion";
 
-
 const ReviewContainer = () => {
-
-  
   const [windowWidth, setWindowWidth] = useState(3);
 
   useEffect(() => {
@@ -57,52 +54,60 @@ const ReviewContainer = () => {
       text: "Quality and simplicity at its finest. With MaidSimpl, I've finally found a service that meets my standards. Their professionals are courteous and thorough.",
       name: "Linda M.",
     },
-    
   ];
   return (
-    <motion.div variants={staggerContainer} initial="hidden" whileInView="show"
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
       id="review-container"
       className="relative w-full h-auto px-6 py-6 md:px-8 md:py-14 lg:px-28 lg:py-14 mt-10 flex flex-col justify-center items-center gap-12 bg-[#EEFEF4]"
     >
-      <img src="/assets/quotes.svg" alt="quotes" className="absolute -top-10 left-10" />
+      <img
+        src="/assets/quotes.svg"
+        alt="quotes"
+        className="absolute -top-10 left-10"
+      />
       <h1 className="text-5xl mt-4 md:mt-2 lg:mt-0 text-center font-extrabold text-black">
-        Head from our clients
+        Hear from our clients
       </h1>
 
       <div className="w-full h-auto flex flex-col md:flex-row gap-4 md:gap-8 lg:gap-12">
-        <motion.div  variants={fadeIn("right", "tween", 0.5, 1)} className="w-full md:w-[50%] lg:w-[40%] xl:w-[30%] flex flex-col gap-6 justify-center items-center ">
+        <motion.div
+          variants={fadeIn("right", "tween", 0.5, 1)}
+          className="w-full md:w-[50%] lg:w-[40%] xl:w-[30%] flex flex-col gap-6 justify-center items-center "
+        >
           <h1 className="text-4xl  text-center md:text-left font-extrabold text-black">
-          Clean Impressions:
-Our Clients 
+            Clean Impressions: Our Clients
             <span className="w-auto border-b-4 border-[#8cd790]"> Speak</span>
           </h1>
           <p className="text-xl text-center md:text-left font-medium text-gray-900">
-          At MaidSimpl, every client's voice matters. We've built more than just a cleaning service; we've built a community of satisfied homeowners who trust our platform's promise of quality and simplicity. But don't just take our word for it.
+            At MaidSimpl, every client's voice matters. We've built more than
+            just a cleaning service; we've built a community of satisfied
+            homeowners who trust our platform's promise of quality and
+            simplicity. But don't just take our word for it.
           </p>
         </motion.div>
 
         <div className="w-full md:w-[50%] lg:w-[60%] xl:w-[68%] h-auto flex gap-4 relative">
-
-        <Swiper
-          spaceBetween={20}
-          modules={[Pagination]}
-          slidesPerView={slidesPerView}
-          className=" mySwiper swiper-h relative"
-          pagination={{
-            clickable: true,
-          }}
-        >
-          {dummyArr.map((item, key) => {
-            return (
-              <SwiperSlide key={key} className="w-full">
-                <Reviewcard text={item.text} name={item.name} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-
+          <Swiper
+            spaceBetween={20}
+            modules={[Pagination]}
+            slidesPerView={slidesPerView}
+            className=" mySwiper swiper-h relative"
+            pagination={{
+              clickable: true,
+            }}
+          >
+            {dummyArr.map((item, key) => {
+              return (
+                <SwiperSlide key={key} className="w-full">
+                  <Reviewcard text={item.text} name={item.name} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
-
       </div>
     </motion.div>
   );
